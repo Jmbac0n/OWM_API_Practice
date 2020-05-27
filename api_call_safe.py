322c2d4b6f2a889db6496964ba35f6e6109faeb1
Jmbac0n <jakematthias@hotmail.com>
2020-05-27 19:47:13 +0100

    Add additional readouts

--- api_call_safe.py
+++ api_call_safe.py
@@ -20,17 +20,20 @@ x = r.json()
 def readout():
 
     celsius_sub = 273.15
-    
+
+    owm_weather = x['weather']
     owm_main = x['main']
     owm_wind = x['wind']
 
+    sky_description = owm_weather[0]
     temp = owm_main['temp']
     fl_temp = owm_main['feels_like']
     wind_spd = owm_wind['speed']
 
     temp_celsius = temp - celsius_sub
     fl_temp_celsius = fl_temp - celsius_sub
-
+    
+    print("Sky status: " + sky_description['main'])
     print("The temperature is: " + "%.2f" % temp_celsius + "C")
     print("The temperature feels like: " + "%.2f" % fl_temp_celsius + "C")
     print("Wind speed is: " + (str(wind_spd)) + "m/s")
